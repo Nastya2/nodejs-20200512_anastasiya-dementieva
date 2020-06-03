b8c8ebb50873eb602ac25a688637e62cf2d8f3f9
Nastya2 <you@example.com>
2020-06-03 20:52:47 +0300

    added 1-task module-2

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -1,14 +1,28 @@
 const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
+const fs = require('fs');
+
 
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.max = options;
+    this.count = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.count += chunk.length;
+    if (this.count > this.max) {
+      callback(new LimitExceededError());
+    } else {
+      callback(null, chunk);
+    }
   }
 }
+
+const limitedStream = new LimitSizeStream();
+const outStream = fs.createWriteStream('out.txt');
+
+limitedStream.pipe(outStream);
 
 module.exports = LimitSizeStream;
